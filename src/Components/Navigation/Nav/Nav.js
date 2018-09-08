b337380346b17b85885b904b6c29d44d5ea2df30
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-09-07 20:26:36 -0500

    Basic responsive navbar working with sliding effect

--- src/Components/Navigation/Nav/Nav.js
+++ src/Components/Navigation/Nav/Nav.js
@@ -2,14 +2,39 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class Nav extends Component {
+    constructor () {
+        super();
+        this.state = {
+            showNav: false
+        }
+    }
+
+    toggleNav = () => {
+        this.setState({
+            showNav: !this.state.showNav
+        })
+    }
+
     render () {
         return (
             <div className="navbar">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/portfolio">Portfolio</Link>
-                <Link to="/pricing">Pricing</Link>
-                <Link to="/contact">Contact</Link>
+                <div className="navbar-bar">
+                    <div className="hamburger-icon" onClick={this.toggleNav}>
+                        <i className="fas fa-bars fa-3x"></i>
+                    </div>
+                    <Link className="link" to="/">Home</Link>
+                    <Link className="link" to="/about">About</Link>
+                    <Link className="link" to="/portfolio">Portfolio</Link>
+                    <Link className="link" to="/pricing">Pricing</Link>
+                    <Link className="link" to="/contact">Contact</Link>
+                </div>
+                <div className={this.state.showNav ? "navbar-dropdown" : "navbar-dropdown hide-nav"}>
+                    <Link className="link" to="/">Home</Link>
+                    <Link className="link" to="/about">About</Link>
+                    <Link className="link" to="/portfolio">Portfolio</Link>
+                    <Link className="link" to="/pricing">Pricing</Link>
+                    <Link className="link" to="/contact">Contact</Link>
+                </div>
             </div>
         )
     }

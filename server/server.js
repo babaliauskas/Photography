df2083cdea60793d6d2f47cbf2cd6514e9e85a77
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-09-27 16:47:07 -0600

    contact page

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')  
+const nodemailer = require('nodemailer')
 require('dotenv').config()
 
 
@@ -8,6 +9,48 @@ const app = express();
 app.use(bodyParser.json())
 
 app.use(express.static( `${__dirname}/../build`))
+
+
+app.post('/api/form', (req, res) => {
+    console.log('tesssss: ')
+   let {name, email, message} = req.body;
+   const output = `
+    Job 
+    Contact Details
+        Name: ${name}
+        email: ${email}
+    
+    Message:
+    ${message}
+    `;
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PSW
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+    let mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: 'Message from babaliauskasprojects.com',
+        text: output
+    };
+ 
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.sendStatus(200);
+        }
+    });
+})
 
 app.listen(process.env.SERVER_PORT, () =>  console.log(`Server listening on ${process.env.SERVER_PORT}`))
 
